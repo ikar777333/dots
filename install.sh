b71220c49b545756d071eccbfcc7ebfde8518d97
ikar <ikar777333@gmail.com>
2019-04-24 18:33:00 +0300

    Added backup save functional for install script

--- install.sh
+++ install.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/bash
 
-for d in `ls .`;
+mkdir $HOME/dots_backup
+
+for d in $(find . -mindepth 2 -type f);
 do
-    ( stow $d )
+    v1="$d"
+    [[ $v1 =~ ./(.*)/(\..*) ]]
+    v2=${BASH_REMATCH[1]}
+    v3=${BASH_REMATCH[2]}
+
+    if[ -e "$HOME/$v3"];
+    then 
+        mkdir -p $HOME/dots_backup/$v3
+        mv $HOME/$v3* $HOME/dots_backup/$v3
+    fi
+    stow $v3
 done
