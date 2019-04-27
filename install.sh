2c160eef7c791fb95d1a758ac7909c96e7035f84
ikar <ikar777333@gmail.com>
2019-04-27 10:10:57 +0300

    fix install script

--- install.sh
+++ install.sh
@@ -9,15 +9,15 @@ do
     v2=${BASH_REMATCH[1]}
     v3=${BASH_REMATCH[2]}
 
-    if[ -e "$HOME/$v3"];
+    if [ -e $HOME/$v3 ]
     then 
         mkdir -p $HOME/dots_backup/$v3
-        if[ -f $HOME/$v3]
+        if [ -f $HOME/$v3 ]
         then
             mv $HOME/$v3 $HOME/dots_backup/$v3
         else
             mv $HOME/$v3/* $HOME/dots_backup/$v3
         fi
     fi
-    stow $v3
+    stow $v2
 done
